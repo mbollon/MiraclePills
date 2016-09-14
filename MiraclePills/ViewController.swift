e350991c47d865f65ae42cd6add2301bcd873ce1
Marc Bollon <marc@infocovery.com>
2016-09-13 20:56:38 -0500

    Miracle Pills Exercise Solution
    This is my solution to the Miracle Pills app in the iOS10 Beginner to
    Paid Professional course by Devslopes.  I created two views placing
    controls inside those views.  Then I only had to hide/unhide the views
    instead of each control.

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -2,17 +2,31 @@
 //  ViewController.swift
 //  MiraclePills
 //
+//  First project in iOS 10 from Beginner to Paid Professional
+//
 //  Created by Marc Bollon on 9/12/16.
 //  Copyright © 2016 Marc Bollon. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var buyBtn: UIButton!
+    @IBOutlet weak var hideView: UIView!
+    @IBOutlet weak var bottomViewContainer: UIView!
+    
+    var states = ["Arkansas,", "Arizona"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        bottomViewContainer.isHidden = true
+        statePicker.isHidden = false
+    }
+    
+    @IBAction func buyBtnPressed(_ sender: AnyObject) {
+        hideView.isHidden = false
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        bottomViewContainer.isHidden = false
+    }
 }
 
